f0b24970725e5b18071980fe00e4ba365b8dc8a0
Michael Sheridan <micksheridan.24@gmail.com>
2019-01-04 20:06:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,21 @@ var takeANumber = function(line, name){
 var nowServing = function (line){
   
   if (line.length === 0){
-    return ""
+    return "There is nobody waiting to be served!"
   }
+  else{
   return line[0];
   line.shift();
+  }
+}
+var currentLine = function (line){
+  
+  let str =  ("The line is currently: ");
+  
+  for (let x = 0; x < line.length; x++){
+    
+    str += (x + ". ");
+    str += (line[x]);
+    if 
+  }
 }
